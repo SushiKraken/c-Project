31d3ef0c552a7568d3dbcbb25cf44fd6d112ca2b
SushiKraken <dolcan@o2.pl>
2016-06-12 20:36:01 +0000

    Add files via upload

---
+++ Gamez2/Bullet.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+
+namespace Gamez2
+{
+    /// <summary>
+    /// Interaction logic for Bullet.xaml
+    /// </summary>
+    public partial class Bullet : UserControl
+    {
+       
+        public Bullet
+       ()
+        {
+            InitializeComponent();
+            
+
+        }
+    }
+}
